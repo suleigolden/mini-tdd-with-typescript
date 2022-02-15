e0ae47ea0b24a1ea553227e33ad8886be128bf31
Suleiman Mamman <suleimamman@gmail.com>
2022-02-15 13:11:52 +0100

    clean up and readme.md file created

---
+++ README.md
@@ -0,0 +1,6 @@
+# A mini TDD with Node, Jest, TypeScript and Mongoose
+
+## Description
+* Testing controller function and logic  with Jest
+* Typing  custom properties on Mongoose models
+* Typing methods on Mongoose models
--- src/models/homeowner.model.ts
+++ src/models/homeowner.model.ts
@@ -58,6 +58,5 @@ HomeownerSchema.methods.comparePassword = async function (
   return bcrypt.compare(candidatePassword, homeOwner.password).catch((e) => false);
 };
 
-// export default mongoose.model<HomeownerDocument>("Homeowner", HomeownerSchema);
 const HomeownerModel = mongoose.model<HomeownerDocument>("Homeowner", HomeownerSchema);
 export default HomeownerModel;
